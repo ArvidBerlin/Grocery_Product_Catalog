fb04b16c2bf12ac8540926247bc81de2aecea325
Arvid Berlin <arvidberlinjobb@gmail.com>
2024-09-30 20:24:27 +0200

    Skapade en mapp och lade till klassen ProductService_Tests i Recourses.Tests

---
+++ Resources.Tests/UnitTests/ProductService_Tests.cs
@@ -0,0 +1,10 @@
+﻿using Moq;
+using Resources.Interfaces;
+using Resources.Models;
+
+namespace Resources.Tests.UnitTests;
+
+public class ProductService_Tests
+{
+    private readonly Mock<IProductService<Product, Product>> _mockProductService = new();
+}
